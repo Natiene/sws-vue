fe56897c0ad211f9e785a31391afd1b0546dc586
Natiene <naty_lockheart@hotmail.com>
2018-03-22 00:33:33 -0300

    Contenst get ok

---
+++ src/components/Contents.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    {{mostrar}}
+
+
+  </div>
+
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: "contents",
+    data() {
+      return {
+        'mostrar': [],
+      }
+
+    },
+    created() {
+      axios.get("https://swapi.co/api/people/1").then(response => {
+        this.mostrar = response.data
+        console.log(response)
+
+      })
+    }
+  }
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
